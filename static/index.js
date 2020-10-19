9659225985a507a4ab52c896a9e8f546043a4ef4
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-19 16:16:18 -0700

    js and html refactoring

--- static/index.js
+++ static/index.js
@@ -9,64 +9,65 @@ const $formSubmit = $("form").on("submit", cupcakeSubmitForm);
 const $deleteCupcake = $cupcakeList.on("click", "li", deleteCupcake);
 
 async function getCupcakeList() {
-    const resp = await axios({
-        url: BASE_URL,
-        method: "GET"
-    })
-    list = resp.data.cupcakes;
+  const resp = await axios({
+    url: BASE_URL,
+    method: "GET"
+  })
+  list = resp.data.cupcakes;
 
-    return list;
+  return list;
 }
 
 async function addCupcakesToDOM() {
-    
-    let list = await getCupcakeList();
 
-    $cupcakeList.empty();
+  let list = await getCupcakeList();
 
-    for (cake of list) {
-        $cupcakeList.append($(`<li class="list-group-item cupcakes" id="${cake.id}">${cake.flavor}</li>`));
-    }
+  $cupcakeList.empty();
+
+  for (cake of list) {
+    $cupcakeList.append($(`<li class="list-group-item cupcakes" id="${cake.id}">${cake.flavor}</li>`));
+  }
 }
 
 
 async function cupcakeSubmitForm(evt) {
-        evt.preventDefault();
-        let flavor = $("#cupcake-flavor").val()
-        let size = $("#cupcake-size").val()
-        let rating = $("#cupcake-rating").val()
-        let image = $("#img-url").val()
+  evt.preventDefault();
+  let flavor = $("#cupcake-flavor").val();
+  let size = $("#cupcake-size").val();
+  let rating = $("#cupcake-rating").val();
+  let image = $("#img-url").val();
 
-        let response = await axios({
-            url: BASE_URL,
-            method: "POST",
-            data: {
-                "flavor": flavor,
-                "size": size,
-                "rating": rating,
-                "image": image
-            }
-        })
+  let response = await axios({
+    url: BASE_URL,
+    method: "POST",
+    data: {
+      flavor,
+      size,
+      rating,
+      image
+    }
+  });
 
-        let newCupcake = response.data.cupcake
-        // console.log(response)
-        showNewCupcake(newCupcake);
-        evt.target.reset()
+  let newCupcake = response.data.cupcake;
+  // console.log(response)
+  // showNewCupcake(newCupcake);
+  addCupcakesToDOM();
+  evt.target.reset();
 }
 
-function showNewCupcake(newCupcake) {
-    $cupcakeList.append($(`<li class="list-group-item" id="${newCupcake.id}">${newCupcake.flavor}</li>`));
-}
+// function showNewCupcake(newCupcake) {
+//     $cupcakeList.append($(`<li class="list-group-item" id="${newCupcake.id}">${newCupcake.flavor}</li>`));
+// }
 
 async function deleteCupcake(evt) {
-    let cupcakeId = this.id;
+  let cupcakeId = this.id;
 
-    let response = await axios({
-        url: `${BASE_URL}/${cupcakeId}`,
-        method: "DELETE"
-    })
+  let response = await axios({
+    url: `${BASE_URL}/${cupcakeId}`,
+    method: "DELETE"
+  })
 
-    addCupcakesToDOM();
+  addCupcakesToDOM();
 }
 
 addCupcakesToDOM();
