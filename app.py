5b10c97a46ebf842eecfa7e5729e5c0df6da608a
mackcooper1408 <44388848+mackcooper1408@users.noreply.github.com>
2020-10-19 08:36:56 -0700

    added initial cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret'
+
+connect_db(app)
+
+debug = DebugToolbarExtension(app)
